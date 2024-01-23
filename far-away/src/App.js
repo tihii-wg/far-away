c277cce90e9c2718be16b65ac482f0e1947e43fd
Vova <podolian@gmail.com>
2024-01-23 22:57:40 +0200

    new

--- far-away/src/App.js
+++ far-away/src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      far away
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackageList />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+function Logo() {
+  return <h1>⛵ Far Away 🌴</h1>;
+}
+
+function Form() {
+	return <div>
+	  <h3>What do you need for you trip? 🚢</h3>
+  </div>;
+}
+
+function PackageList() {
+  return <div className="list">list</div>;
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>You have 4 items on your list,and you already packed 0(0%)</p>
+    </footer>
+  );
+}

694887b170173e96fc04886097d2576976ccae19
Vova <podolian@gmail.com>
2024-01-28 13:18:00 +0200

    new

--- far-away/src/Form.js
+++ far-away/src/Form.js
@@ -1,13 +1,16 @@
 export function Form() {
   return (
     <form className="form">
-		  <h3>What do you need for you trip? 🚢</h3>
-		  <select>
-			  <option value={1}>1</option>
-			  <option value={2}>2</option>
-			  <option value={3}>3</option>
-		  </select>
-		  <input type="text" placeholder="Item..."/>
+      <h3>What do you need for you trip? 🚢</h3>
+      <select>
+        {Array.from({ length: 20 }, (v, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button type="submit">ADD</button>
     </form>
   );
 }

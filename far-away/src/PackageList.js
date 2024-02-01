40981179832d41cd7b9fd9f531b4e845379f982c
Vova <podolian@gmail.com>
2024-02-01 21:56:42 +0200

    new

--- far-away/src/PackageList.js
+++ far-away/src/PackageList.js
@@ -2,15 +2,21 @@ import { Item } from "./Item";
 
 export function PackageList({ items, toogleIsPacked, remooveItem }) {
   return (
-    <ul className="list">
-      {items.map((item) => (
-        <Item
-          key={item.id}
-          item={item}
-          toogleIsPacked={toogleIsPacked}
-          remooveItem={remooveItem}
-        />
-      ))}
-    </ul>
+    <>
+      <ul className="list">
+        {items.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            toogleIsPacked={toogleIsPacked}
+            remooveItem={remooveItem}
+          />
+        ))}
+      </ul>
+      <div className="buttons">
+        <button >reset</button>
+        <select >sort by</select>
+      </div>
+    </>
   );
 }
